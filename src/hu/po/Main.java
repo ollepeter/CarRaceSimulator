fb750d0b850284688d18906c50266da9622e3907
ollepeter <mr.peterolle@gmail.com>
2017-08-24 13:46:20 +0200

    Set isRaining's according to the 30% probability

--- src/hu/po/Main.java
+++ src/hu/po/Main.java
@@ -1,8 +1,47 @@
+
+
 package hu.po;
 
+
 public class Main {
+    public static boolean isRaining = false;
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        System.out.println("This is the main method\n");
+
+        createVehicle();
+        simulateRace();
+        printRaceResult();
     }
+
+
+
+    private static void createVehicle() {
+        System.out.println("This is createVehicle\n");
+
+    }
+
+
+
+    private static void simulateRace() {
+        System.out.println("This is simulateRace");
+        isRaining = (int)(Math.random()*100) <= 30; // "30" is the predefined probability"
+        System.out.println("Is raining -> " + isRaining + "\n");
+    }
+
+
+
+    private static void printRaceResult() {
+        System.out.println("This is printRaceResult\n");
+    }
+
+
 }
+
+
+
+
+
+
